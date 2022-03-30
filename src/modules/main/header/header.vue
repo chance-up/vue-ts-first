bd3a614929ca2d6be4664a045cc56b6a78465344
chance-up <ccs6473@naver.com>
2022-03-30 14:08:45 +0900

    ✨ add fontawesome cdn #3

--- src/modules/main/header/header.vue
+++ src/modules/main/header/header.vue
@@ -2,24 +2,12 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">Navbar</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-          <a class="nav-link" href="#">Features</a>
-          <a class="nav-link" href="#">Pricing</a>
-          <a class="nav-link disabled">Disabled</a>
-        </div>
+      <div class="navbar-nav">
+        <button class="nav-link" role="button">
+          <i class="fas fa-bars"></i>
+        </button>
+        <router-link to="/dashboard">Dashboard</router-link>
+        <router-link to="/search">Search</router-link>
       </div>
     </div>
   </nav>
